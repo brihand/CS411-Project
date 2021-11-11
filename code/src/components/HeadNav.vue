ac0652d487a4c5190f16fba0f1af51df5b324846
kunlins <906234621@qq.com>
2021-11-11 22:49:06 +0800

    kunlin song

---
+++ code/src/components/HeadNav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="head">
+    <div class="d-flex justify-content-between">
+      <div class="left">ServerManager</div>
+      <div class="right">
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            Welcome<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>username</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+.head{
+  background-color:#324057;
+  color: #fff;
+  padding: 20px;
+  .right{
+    span{
+      color: #fff;
+    }
+  }
+}
+</style>
---
+++ code/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <Head />
+    <el-row>
+      <el-col :span="4">
+        <LeftMenu />
+      </el-col>
+      <el-col :span="20">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Head from '../components/HeadNav.vue'
+import LeftMenu from '../components/LeftMenu.vue'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+  },
+  components: {
+    Head,
+    LeftMenu
+  }
+}
+</script>
